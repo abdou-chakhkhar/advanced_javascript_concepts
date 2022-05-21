d5110b150897499ea92720f5fc592d0cb5600ec0
abdou-chakhkhar <67806530+abdou-chakhkhar@users.noreply.github.com>
2022-05-21 15:09:33 +0100

    Update linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -43,12 +43,54 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList(){
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value){
+    if(index >= this.length) this.append(value);
+
+    const newNode = {
+      value,
+      next: null
+    }
+
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+    
+  }
+
+  traverseToIndex(index){
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+//myLinkedList.insert(888, 88)
+myLinkedList.insert(2, 99)
+myLinkedList.printList();
 
 
-console.log(myLinkedList.head.next)
+ console.log(myLinkedList.printList())
+//console.log(myLinkedList.insert(1, 3));
